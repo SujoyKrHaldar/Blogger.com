1463891b5d588dc52e8fd13e79d0015d52734eba
SujoyKrHaldar <sujoykrhaldar@gmail.com>
2023-12-17 04:01:23 +0530

    Update : Temporary UI

--- src/pages/public/Feed.jsx
+++ src/pages/public/Feed.jsx
@@ -1,24 +1,24 @@
-import { ArticleList, MetaTags } from "../../components";
-import { useEffect, useState } from "react";
+import { ArticleList, MetaTags, Image } from "../../components";
+// import { useEffect, useState } from "react";
 
 function Feed() {
-  const [data, setData] = useState([]);
+  // const [data, setData] = useState([]);
 
-  const fetchData = async () => {
-    // API LINK: https://www.slingacademy.com/article/sample-blog-posts-public-rest-api-for-practice/
-    const response = await fetch(
-      "https://api.slingacademy.com/v1/sample-data/blog-posts?limit=15"
-    );
+  // const fetchData = async () => {
+  //   // API LINK: https://www.slingacademy.com/article/sample-blog-posts-public-rest-api-for-practice/
+  //   const response = await fetch(
+  //     "https://api.slingacademy.com/v1/sample-data/blog-posts?limit=15"
+  //   );
 
-    if (response.ok) {
-      const { blogs } = await response.json();
-      setData(blogs);
-    }
-  };
+  //   if (response.ok) {
+  //     const { blogs } = await response.json();
+  //     setData(blogs);
+  //   }
+  // };
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
 
   return (
     <>
@@ -28,8 +28,14 @@ function Feed() {
         description="Discover stories, thinking, and expertise from writers on any topic. Create and grow your developer blog, newsletter, or team engineering blog effortlessly with us."
       />
 
-      <section className="min-h-full">
-        <ArticleList data={data} />
+      <section className="py-24 flex items-center justify-center">
+        {/* <ArticleList data={data} /> */}
+        <div className="text-center space-y-4 ">
+          <div className="w-[500px] h-auto mx-auto">
+            <Image src="/comment.png" />
+          </div>
+          <h2 className="text-5xl font-bold">Coming soon</h2>
+        </div>
       </section>
     </>
   );
