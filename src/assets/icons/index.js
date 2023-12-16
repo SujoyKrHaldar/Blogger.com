313146d1ee5ee5b593f660a17edd83655ceba453
SujoyKrHaldar <sujoykrhaldar@gmail.com>
2023-12-17 03:12:41 +0530

    Setup Icons

---
+++ src/assets/icons/index.js
@@ -0,0 +1,21 @@
+import { MdError as IconError } from "react-icons/md";
+import { TiTick as IconSuccess } from "react-icons/ti";
+import {
+  IoMdClose as IconClose,
+  IoIosWarning as IconWarning,
+} from "react-icons/io";
+import { AiFillLock as IconLock } from "react-icons/ai";
+import {
+  AiFillEyeInvisible as IconEyeInvisible,
+  AiFillEye as IconEyeOpen,
+} from "react-icons/ai";
+
+export {
+  IconError,
+  IconClose,
+  IconLock,
+  IconEyeInvisible,
+  IconEyeOpen,
+  IconWarning,
+  IconSuccess,
+};
