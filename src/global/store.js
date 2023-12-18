b00ad5517e3aa7ceb3d756a9af287451dfc8b9d6
SujoyKrHaldar <sujoykrhaldar@gmail.com>
2023-12-18 19:51:53 +0530

    Setup new Profile Reducer

--- src/global/store.js
+++ src/global/store.js
@@ -1,11 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-
 import { authReducers } from "./slices/authSlice";
 import { notificationReducer } from "./slices/notificationSlice";
+import { profileReducers } from "./slices/profileSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducers,
+    profile: profileReducers,
     notification: notificationReducer,
   },
 });
