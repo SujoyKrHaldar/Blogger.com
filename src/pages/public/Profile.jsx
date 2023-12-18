f48e752e1d2c91a19d5d8be0134069701cfe525c
SujoyKrHaldar <sujoykrhaldar@gmail.com>
2023-12-19 01:54:09 +0530

    Update : Add custom hook : fetching user profile

--- src/pages/public/Profile.jsx
+++ src/pages/public/Profile.jsx
@@ -1,5 +1,4 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import {
   MetaTags,
@@ -7,41 +6,12 @@ import {
   ProfileHeader,
   SplashScreen,
 } from "../../components";
-import { userProfile } from "../../service";
+import { useFetchProfile } from "../../hooks";
 import PageNotFound from "./PageNotFound";
 
 function Profile() {
   const { username } = useParams();
-
-  const [profile, setProfile] = useState({});
-  const [notFound, setNotFound] = useState(false);
-  const [loading, setLoading] = useState(true);
-
-  // TODO:
-  // 3. While accessing this component through /author/:username, just fetch data in this component only using
-  //    useEffect or react-query ( in future) fetch data from query parameter ( :username )
-
-  const getUserProfile = async () => {
-    if (!username) {
-      setNotFound(true);
-      return;
-    }
-    try {
-      const { documents } = await userProfile.getProfileByUsername(username);
-
-      if (documents[0]) {
-        setProfile(documents[0]);
-      } else {
-        setNotFound(true);
-      }
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    getUserProfile();
-  }, [username]);
+  const { profile, loading, notFound } = useFetchProfile(username);
 
   return (
     <>
