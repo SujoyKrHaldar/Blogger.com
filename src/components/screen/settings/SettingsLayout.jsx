4dde1f9411e2b9681c7fd051425b5db14d58028d
SujoyKrHaldar <sujoykrhaldar@gmail.com>
2023-12-20 02:22:12 +0530

    Update : fix settings UI

--- src/components/screen/settings/SettingsLayout.jsx
+++ src/components/screen/settings/SettingsLayout.jsx
@@ -3,8 +3,8 @@ import SettingsNavbar from "./SettingsNavbar";
 
 function SettingsLayout({ children, tab }) {
   return (
-    <section className="container w-full h-screen py-0 flex gap-4">
-      <div className="flex-1 max-w-[280px]">
+    <section className="container w-full h-screen py-0 flex gap-4 overflow-auto">
+      <div className="flex-1 max-w-[280px] sticky top-0">
         <SettingsNavbar tab={tab} />
       </div>
       <div className="flex-1">{children}</div>
--- src/components/screen/settings/SettingsTemplate.jsx
+++ src/components/screen/settings/SettingsTemplate.jsx
@@ -1,10 +1,10 @@
 /* eslint-disable react/prop-types */
 function SettingsTemplate({ title, children }) {
   return (
-    <section className="pt-28 px-8 space-y-4">
+    <div className="pt-28 pb-16 px-8 space-y-4">
       <h1 className="text-3xl font-bold">{title}</h1>
       {children}
-    </section>
+    </div>
   );
 }
 
