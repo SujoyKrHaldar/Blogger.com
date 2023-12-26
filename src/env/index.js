27d53741703210b0a9e5ea280b315c96b79d23fb
SujoyKrHaldar <sujoykrhaldar@gmail.com>
2023-12-27 01:13:49 +0530

    Update : update envs

--- src/env/index.js
+++ src/env/index.js
@@ -2,11 +2,13 @@ const config = {
   appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
   appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
   appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
-  appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+
   appwriteUserCollectionId: String(
     import.meta.env.VITE_APPWRITE_USER_COLLECTION_ID
   ),
-  // testCollectionId: String(import.meta.env.VITE_APPWRITE_TEST_COLLECTION_ID),
+  appwritePostCollectionId: String(
+    import.meta.env.VITE_APPWRITE_POST_COLLECTION_ID
+  ),
   appwriteUserProfilePicStorageId: String(
     import.meta.env.VITE_APPWRITE_USER_PROFILE_PIC_STORAGE_ID
   ),
