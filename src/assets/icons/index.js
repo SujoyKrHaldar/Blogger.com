e157660e3def789615a68087fcef4cb86bcd75cc
SujoyKrHaldar <sujoykrhaldar@gmail.com>
2023-12-27 01:03:24 +0530

    Update : Add new icons

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,8 +1,10 @@
 import {
   AiFillEyeInvisible as IconEyeInvisible,
-  AiFillEye as IconEyeOpen, AiFillLock as IconLock
+  AiFillEye as IconEyeOpen,
+  AiFillLock as IconLock,
 } from "react-icons/ai";
 import {
+  IoIosArrowRoundForward as IconArrowLeft,
   IoMdClose as IconClose,
   IoIosWarning as IconWarning,
 } from "react-icons/io";
@@ -10,6 +12,13 @@ import { MdError as IconError } from "react-icons/md";
 import { TiTick as IconSuccess } from "react-icons/ti";
 
 export {
-  IconClose, IconError, IconEyeInvisible,
-  IconEyeOpen, IconLock, IconSuccess, IconWarning
+  IconArrowLeft,
+  IconClose,
+  IconError,
+  IconEyeInvisible,
+  IconEyeOpen,
+  IconLock,
+  IconSuccess,
+  IconWarning
 };
+
