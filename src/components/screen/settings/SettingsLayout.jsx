f30aecd5aaffda92295c9b59808c4f52b3069f08
SujoyKrHaldar <sujoykrhaldar@gmail.com>
2023-12-27 16:39:09 +0530

    Update : fix issues

--- src/components/screen/settings/SettingsLayout.jsx
+++ src/components/screen/settings/SettingsLayout.jsx
@@ -1,11 +1,11 @@
 /* eslint-disable react/prop-types */
 import SettingsNavbar from "./SettingsNavbar";
 
-function SettingsLayout({ children, tab }) {
+function SettingsLayout({ children }) {
   return (
     <section className="container w-full h-screen py-0 flex gap-4 overflow-auto">
       <div className="flex-1 max-w-[280px] sticky top-0">
-        <SettingsNavbar tab={tab} />
+        <SettingsNavbar />
       </div>
       <div className="flex-1">{children}</div>
     </section>
