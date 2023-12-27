56a8bcf5adc0ac2dcbfa8db772cf38fe8441ae65
SujoyKrHaldar <sujoykrhaldar@gmail.com>
2023-12-27 16:38:21 +0530

    Update : update nested settings routes & add custom navlink

--- src/components/screen/settings/SettingsNavbar.jsx
+++ src/components/screen/settings/SettingsNavbar.jsx
@@ -1,30 +1,29 @@
-/* eslint-disable react/prop-types */
-import { Link } from "react-router-dom";
+import CustomNavLink from "../../shared/CustomNavLink";
 
 const links = [
   {
     name: "Profile",
-    // tab: "profile",
+    url: "/settings",
   },
   {
     name: "Branding",
-    tab: "branding",
+    url: "/settings/branding",
   },
   {
     name: "Account",
-    tab: "account",
+    url: "/settings/account",
   },
   {
     name: "Social Links",
-    tab: "social",
+    url: "/settings/social",
   },
   {
     name: "Sessions",
-    tab: "session",
+    url: "/settings/session",
   },
 ];
 
-function SettingsNavbar({ tab }) {
+function SettingsNavbar() {
   return (
     <div className="pt-28 pb-8 pl-1 w-full h-full flex flex-col justify-between border-r border-black">
       <div className="space-y-6">
@@ -32,34 +31,12 @@ function SettingsNavbar({ tab }) {
 
         <nav className="space-y-2">
           {links.map((data) => (
-            <Link
-              key={data.name}
-              className={`block py-2 duration-200 ease-in-out 
-              ${
-                data.tab === tab
-                  ? "border-r-4 border-green-700 text-green-700 font-medium pl-6 bg-green-100 rounded-l-lg"
-                  : "border-transparent"
-              } 
-              `}
-              to={data.tab ? `/settings?tab=${data.tab}` : "/settings"}
-            >
+            <CustomNavLink className="block py-2" key={data.name} to={data.url}>
               <p>{data.name}</p>
-            </Link>
+            </CustomNavLink>
           ))}
         </nav>
       </div>
-
-      <Link
-        className={`text-red-600 font-bold block py-2 duration-200 ease-in-out
-               ${
-                 tab === "deactivate"
-                   ? "border-r-4 border-red-700 text-green-700 pl-6 bg-red-200 rounded-l-lg"
-                   : "border-transparent"
-               } `}
-        to="/settings?tab=deactivate"
-      >
-        <p>Delete Account</p>
-      </Link>
     </div>
   );
 }
