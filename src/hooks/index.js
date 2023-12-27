0337a2e3822d8103ede95b95a27cbdaf49129289
SujoyKrHaldar <sujoykrhaldar@gmail.com>
2023-12-27 21:08:29 +0530

    Setup new hook for article fetching

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,5 +1,11 @@
 import useAutoLoginOnRefresh from "./useAutoLoginOnRefresh";
+import useFetchPostBySlug from "./useFetchPostBySlug";
 import useFetchPosts from "./useFetchPosts";
 import useFetchProfile from "./useFetchProfile";
 
-export { useAutoLoginOnRefresh, useFetchPosts, useFetchProfile };
+export {
+  useAutoLoginOnRefresh,
+  useFetchPosts,
+  useFetchProfile,
+  useFetchPostBySlug,
+};
---
+++ src/hooks/useFetchPostBySlug.js
@@ -0,0 +1,32 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from "react";
+import { postService } from "../service";
+
+function useFetchPostBySlug(slug) {
+  const [post, setPost] = useState([]);
+  const [notFound, setNotFound] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  const getArticles = async () => {
+    try {
+      const { documents } = await postService.getPostBySlug(slug);
+      if (documents[0]) {
+        setPost(documents[0]);
+      } else {
+        setNotFound(true);
+      }
+    } catch (error) {
+      setNotFound(true);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getArticles();
+  }, []);
+
+  return { loading, notFound, post };
+}
+
+export default useFetchPostBySlug;
