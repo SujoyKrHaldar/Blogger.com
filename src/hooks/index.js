f07fb4628229c17808a102acabec462a51168fd4
SujoyKrHaldar <sujoykrhaldar@gmail.com>
2023-12-30 00:50:42 +0530

    Update : add new hooks

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -2,10 +2,14 @@ import useAutoLoginOnRefresh from "./useAutoLoginOnRefresh";
 import useFetchPostBySlug from "./useFetchPostBySlug";
 import useFetchPosts from "./useFetchPosts";
 import useFetchProfile from "./useFetchProfile";
+import useSearchPost from "./useSearchPost";
+import useSearchProfile from "./useSearchProfile";
 
 export {
   useAutoLoginOnRefresh,
   useFetchPosts,
   useFetchProfile,
   useFetchPostBySlug,
+  useSearchPost,
+  useSearchProfile,
 };
