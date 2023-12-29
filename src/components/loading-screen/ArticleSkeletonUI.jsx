eda1be69bc4c5059eb4cce066ffef12e28c89726
SujoyKrHaldar <sujoykrhaldar@gmail.com>
2023-12-29 20:44:58 +0530

    Update : update UI

--- src/components/loading-screen/ArticleSkeletonUI.jsx
+++ src/components/loading-screen/ArticleSkeletonUI.jsx
@@ -1,12 +1,13 @@
 function ArticleSkeletonUI() {
   return (
-    <section className="py-32 space-y-14 fixed inset-0 z-40 bg-white">
+    <section className="py-32 space-y-14 fixed inset-0 z-40 bg-white opacity-80">
       <div className="container">
         <div className="space-y-3">
           <div className="max-w-[180px] h-[25px] mx-auto bg-gray-300 animate-pulse rounded-xl"></div>
           <div className="max-w-[900px] h-[70px] mx-auto bg-gray-300 animate-pulse rounded-xl"></div>
           <div className="max-w-[1000px] h-[70px] mx-auto bg-gray-300 animate-pulse rounded-xl"></div>
           <div className="max-w-[250px] h-[25px] mx-auto bg-gray-300 animate-pulse rounded-xl"></div>
+          <div className="max-w-[250px] h-[43px] mx-auto bg-gray-300 animate-pulse rounded-xl"></div>
         </div>
       </div>
       <div className="w-full h-[350px] overflow-hidden bg-gray-300 animate-pulse rounded-t-[100px]"></div>
