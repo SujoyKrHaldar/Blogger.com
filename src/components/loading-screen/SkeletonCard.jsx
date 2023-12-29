e81ed0c4a6a523298b5211de3a2374dbcf7afd80
SujoyKrHaldar <sujoykrhaldar@gmail.com>
2023-12-30 00:52:40 +0530

    Update : update UI & add height as props

--- src/components/loading-screen/SkeletonCard.jsx
+++ src/components/loading-screen/SkeletonCard.jsx
@@ -1,6 +1,9 @@
-function SkeletonCard() {
+/* eslint-disable react/prop-types */
+function SkeletonCard({ height = "h-[400px]" }) {
   return (
-    <div className="rounded-xl w-full h-[400px] duration-300 animate-pulse bg-gray-200"></div>
+    <div
+      className={`rounded-xl w-full ${height} duration-300 animate-pulse bg-gray-200`}
+    ></div>
   );
 }
 
