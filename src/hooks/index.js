85f29e99f208679bdc98cfa49e3987e0835336d0
SujoyKrHaldar <sujoykrhaldar@gmail.com>
2023-12-31 11:34:36 +0530

    Update : add new hook useQueryParams

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -2,6 +2,7 @@ import useAutoLoginOnRefresh from "./useAutoLoginOnRefresh";
 import useFetchPostBySlug from "./useFetchPostBySlug";
 import useFetchPosts from "./useFetchPosts";
 import useFetchProfile from "./useFetchProfile";
+import useQueryParams from "./useQueryParams";
 import useSearchPost from "./useSearchPost";
 import useSearchProfile from "./useSearchProfile";
 
@@ -12,4 +13,5 @@ export {
   useFetchPostBySlug,
   useSearchPost,
   useSearchProfile,
+  useQueryParams,
 };
