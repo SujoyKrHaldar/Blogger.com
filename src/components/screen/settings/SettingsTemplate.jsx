329e411558a8584a6516e255402d8d932013f5a5
SujoyKrHaldar <sujoykrhaldar@gmail.com>
2024-01-03 01:37:21 +0530

    Update : add breadcrumb

--- src/components/screen/settings/SettingsTemplate.jsx
+++ src/components/screen/settings/SettingsTemplate.jsx
@@ -1,7 +1,25 @@
 /* eslint-disable react/prop-types */
+import { Link, useNavigate } from "react-router-dom";
+
 function SettingsTemplate({ title, children }) {
+  const navigate = useNavigate();
   return (
     <div className="pt-28 pb-16 px-8 space-y-4">
+      <nav className="flex gap-2 text-gray-500 pt-2">
+        <p
+          onClick={() => navigate(-1)}
+          className="text-sm cursor-pointer hover:text-black duration-300"
+        >
+          Go Back
+        </p>
+        <p className="text-sm">{">"}</p>
+        <Link to="/settings">
+          <p className="text-sm cursor-pointer hover:text-black duration-300">
+            Settings
+          </p>
+        </Link>
+      </nav>
+
       <h1 className="text-3xl font-bold">{title}</h1>
       {children}
     </div>
