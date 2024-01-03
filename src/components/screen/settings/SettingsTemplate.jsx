2da5d11e7ebf868bad0b6a62b569ca7bce22b20a
SujoyKrHaldar <sujoykrhaldar@gmail.com>
2024-01-03 23:05:41 +0530

    Update : update breadcrumb

--- src/components/screen/settings/SettingsTemplate.jsx
+++ src/components/screen/settings/SettingsTemplate.jsx
@@ -3,21 +3,29 @@ import { Link, useNavigate } from "react-router-dom";
 
 function SettingsTemplate({ title, children }) {
   const navigate = useNavigate();
+
   return (
-    <div className="pt-28 pb-16 px-8 space-y-4">
-      <nav className="flex gap-2 text-gray-500 pt-2">
+    <div className="pt-28 pb-16 pl-16 space-y-4">
+      <nav className="flex gap-2 items-center justify-between text-gray-500 pt-2">
+        <div className="flex items-center gap-2">
+          <Link to="/">
+            <p className="text-sm cursor-pointer hover:text-black duration-300">
+              Home
+            </p>
+          </Link>
+          <p className="text-sm">{">"}</p>
+          <Link to="/settings">
+            <p className="text-sm cursor-pointer hover:text-black duration-300">
+              Settings
+            </p>
+          </Link>
+        </div>
         <p
           onClick={() => navigate(-1)}
-          className="text-sm cursor-pointer hover:text-black duration-300"
+          className="text-sm cursor-pointer text-black"
         >
-          Go Back
+          {"<"} Go Back
         </p>
-        <p className="text-sm">{">"}</p>
-        <Link to="/settings">
-          <p className="text-sm cursor-pointer hover:text-black duration-300">
-            Settings
-          </p>
-        </Link>
       </nav>
 
       <h1 className="text-3xl font-bold">{title}</h1>
